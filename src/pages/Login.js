e1a823c182985a2457cda7237a74e92a336636a3
Shikhardeep Meena <shikhar4090@gmail.com>
2025-09-21 14:48:08 +0530

    debug

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,7 +10,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await API.post("auth/login/", { username, password }); // 👈 send username
+      const res = await API.post("/api/auth/login/", { username, password }); // 👈 send username
       localStorage.setItem("access", res.data.access);
       localStorage.setItem("refresh", res.data.refresh);
       navigate("/dashboard");
